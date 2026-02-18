b222d031a8b4a8db40eb10a8012d05e18ffbd0d9
bruno <brunotrodrigues98@gmail.com>
2026-02-18 17:51:46 +0100

    senddata speed

--- microros-gateway.ino
+++ microros-gateway.ino
@@ -20,9 +20,17 @@ uint8_t peerMAC[6] = {0x28, 0x05, 0xA5, 0x26, 0xFB, 0x28};
 
 rcl_node_t node;
 rcl_publisher_t publisher;
+rcl_publisher_t speed;
 rclc_support_t support;
 rcl_allocator_t allocator;
+
+
+rclc_executor_t executor;
+rcl_subscription_t subscriber;
 std_msgs__msg__Float64 msg;
+
+float speedRight;
+float speedLeft;
 
 // ========== SETUP FUNCTION ==========
 void setup() {
@@ -64,6 +72,12 @@ void setup() {
     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float64),
     "distanceProject"
   );
+  rclc_publisher_init_default(
+    &speed,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float64),
+    "sendspeed"
+  );
   Serial.println("Micro-ROS initialized");
 }
 
@@ -73,7 +87,24 @@ void loop() {
   delay(1000);
   char msg[16] = "Hello";
   esp_now_send(peerMAC, (uint8_t*)msg, strlen(msg));
+  sendSpeed(speedRight, speedLeft);
 }
+
+void sendSpeed(float speedRight, float speedLeft){
+  char messageBuffer[80];
+
+  int len = snprintf(messageBuffer, sizeof(messageBuffer),
+    "speedRight=%06.2f speedLeft=%+06.2f", // simplify string and create onDataRec
+    speedRight, speedLeft);
+
+  esp_err_t result = esp_now_send(peerMAC, (uint8_t *)messageBuffer, len);
+
+  if (result == ESP_OK){
+    Serial.print("Sent successfully");
+  }
+}
+
+
 
 void OnDataRecv(const esp_now_recv_info* info, const unsigned char* incomingData, int len) {
   
