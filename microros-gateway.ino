0925bb7dd64c05b099d7c29cd1ee7396713d8c6b
bruno <brunotrodrigues98@gmail.com>
2026-02-18 16:39:18 +0100

    add microros

---
+++ microros-gateway.ino
@@ -0,0 +1,101 @@
+// ========== LIBRARIES ==========
+#include <esp_now.h>
+#include <esp_wifi.h>
+#include <WiFi.h>
+
+#include <micro_ros_arduino.h>
+#include <rclc/rclc.h>
+#include <std_msgs/msg/float64.h>
+
+#include <rclc/executor.h>
+#include <std_msgs/msg/bool.h>
+
+// ========== FUNCTION PROTOTYPES ==========
+void OnDataSent(const wifi_tx_info_t* info, esp_now_send_status_t status);
+void OnDataRecv(const esp_now_recv_info* info, const unsigned char* incomingData, int len);
+esp_err_t addPeer(uint8_t* mac, esp_now_peer_info_t* peerInfo);
+
+// Variables
+uint8_t peerMAC[6] = {0x28, 0x05, 0xA5, 0x26, 0xFB, 0x28};
+
+rcl_node_t node;
+rcl_publisher_t publisher;
+rclc_support_t support;
+rcl_allocator_t allocator;
+std_msgs__msg__Float64 msg;
+
+// ========== SETUP FUNCTION ==========
+void setup() {
+  Serial.begin(115200);
+  
+
+  WiFi.begin();
+  esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE);
+
+  esp_now_init();
+
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_register_recv_cb(OnDataRecv);
+
+  addPeer(peerMAC, NULL);
+  
+  // Serial.println("Setup complete");
+  // Serial.print("MAC Address: ");
+  // Serial.println(WiFi.macAddress());
+
+  //   // Initialize micro-ROS
+  set_microros_transports();
+
+  allocator = rcl_get_default_allocator();
+  rclc_support_init(&support, 0, NULL, &allocator);
+
+  
+  rclc_node_init_default(
+    &node,
+    "sonar_node1",
+    "",
+    &support
+  );
+
+  // Create publisher
+  rclc_publisher_init_default(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float64),
+    "distanceProject"
+  );
+  Serial.println("Micro-ROS initialized");
+}
+
+// ========== MAIN LOOP ==========
+void loop() {
+
+  delay(1000);
+  char msg[16] = "Hello";
+  esp_now_send(peerMAC, (uint8_t*)msg, strlen(msg));
+}
+
+void OnDataRecv(const esp_now_recv_info* info, const unsigned char* incomingData, int len) {
+  
+  
+  char msg[len+1] = {0};
+  double number;
+  memcpy(&number, (void*)incomingData, sizeof(double));
+
+  Serial.print("Received data: ");
+  Serial.println(number);
+}
+void OnDataSent(const wifi_tx_info_t* info, esp_now_send_status_t status) {
+  return;
+}
+
+esp_err_t addPeer(uint8_t* mac, esp_now_peer_info_t* peerInfo){
+  if(peerInfo == NULL){
+    peerInfo = (esp_now_peer_info_t*)malloc(sizeof(esp_now_peer_info_t));
+    memset(peerInfo, 0, sizeof(esp_now_peer_info_t));
+  }
+  memcpy(peerInfo->peer_addr, mac, 6);
+  peerInfo->channel = 0;
+  peerInfo->encrypt = false;
+  return esp_now_add_peer(peerInfo);
+}
